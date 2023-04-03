669683d067bf092e39cd810d6a8721ba29d904c3
Wiktor Ogrodnik <wogrodnik@internships.antmicro.com>
2023-04-03 13:47:04 +0200

    Fixes get-image.sh script. Flags to copy

--- scripts/get-image.sh
+++ scripts/get-image.sh
@@ -6,7 +6,7 @@ IMAGE_SOURCE=$2
 if [ $IMAGE_SOURCE_TYPE == 'url' ]; then
     wget -q --no-verbose $IMAGE_SOURCE -O images.tar.xz;
 elif [ $IMAGE_SOURCE_TYPE == 'path' ]; then
-    cp $IMAGE_SOURCE ./images.tar.xz
+    cp -up $IMAGE_SOURCE ./images.tar.xz
 else
     echo "Image source type not supported"
     exit 1
